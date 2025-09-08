fc7e24abbb3d18cb87b9fa2914103d1891d7618c
Vladimir Simanovskiy <vladimir@tvoykarier.ru>
2025-09-09 01:40:00 +0500

    feat(transaction-logger): interface logger is updated, initializeLogger, implementation logger

--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"sync"
 
 	"github.com/gorilla/mux"
+)
+
+///// Event /////
+
+type Event struct {
+	Sequence  uint64
+	EventType EventType
+	Key       string
+	Value     string
+}
+
+type EventType byte
+
+const (
+	_ = iota
+	EventDelete
+	EventPut
 )
 
 //////TransactionLogger/////
@@ -15,21 +35,131 @@ import (
 type TransactionLogger interface {
 	WriteDelete(key string)
 	WritePut(key, value string)
+	Err() <-chan error
+	Run()
+	ReadEvents() (<-chan Event, <-chan error)
 }
 
 /////FileTransactionLogger/////
 
 type FileTransactionLogger struct {
-	// file *os.File
+	events       chan<- Event
+	errors       <-chan error
+	lastSequence uint64
+	file         *os.File
 }
 
 func (l *FileTransactionLogger) WriteDelete(key string) {
-	// fmt.Fprintf(l.file, "DELETE %s\n", key)
+	l.events <- Event{EventType: EventDelete, Key: key}
 }
 
 func (l *FileTransactionLogger) WritePut(key, value string) {
-	// fmt.Fprintf(l.file, "PUT %s %s\n", key, value)
+	l.events <- Event{EventType: EventPut, Key: key, Value: value}
 }
+
+func (l *FileTransactionLogger) Err() <-chan error {
+	return l.errors
+}
+
+func NewFileTransactionLogger(fileName string) (TransactionLogger, error) {
+	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND|os.O_RDWR, os.ModePerm)
+	if err != nil {
+		return nil, fmt.Errorf("cannot open transaction log file: %w", err)
+	}
+	///TODO:: проверить дескриптор файла
+	return &FileTransactionLogger{file: file}, nil
+}
+
+func (l *FileTransactionLogger) Run() {
+	events := make(chan Event, 16)
+	errors := make(chan error, 1)
+	l.events = events
+	l.errors = errors
+
+	go func() {
+		for e := range events {
+			l.lastSequence++
+
+			_, err := fmt.Fprintf(l.file, "%d\t%d\t%s\t%s\n", l.lastSequence, e.EventType, e.Key, e.Value)
+			if err != nil {
+				errors <- err
+				return
+			}
+		}
+	}()
+}
+
+func (l *FileTransactionLogger) ReadEvents() (<-chan Event, <-chan error) {
+	scanner := bufio.NewScanner(l.file)
+	outEvent := make(chan Event)
+	outError := make(chan error, 1)
+
+	go func() {
+		var e Event
+		defer close(outEvent)
+		defer close(outError)
+
+		for scanner.Scan() {
+
+			line := scanner.Text()
+			_, err := fmt.Sscanf(line, "%d\t%d\t%s\t%s", &e.Sequence, &e.EventType, &e.Key, &e.Value)
+			if err != nil {
+				outError <- fmt.Errorf("input parse error %q: %w", line, err)
+				return
+			}
+
+			if l.lastSequence >= e.Sequence {
+				outError <- fmt.Errorf("input parse error %q: sequence not increasing", line)
+				return
+			}
+			l.lastSequence = e.Sequence
+
+			outEvent <- e
+		}
+		if err := scanner.Err(); err != nil {
+			outError <- fmt.Errorf("transaction log read failure: %w", err)
+			return
+
+		}
+	}()
+	return outEvent, outError
+}
+
+func inizializeTransactionLogger() error {
+	var err error
+	logger, err = NewFileTransactionLogger("transaction.log")
+	if err != nil {
+		return fmt.Errorf("failed to create event logger: %w", err)
+	}
+
+	events, errors := logger.ReadEvents()
+	e, ok := Event{}, true
+
+	for ok && err == nil {
+		select {
+		case err, ok = <-errors:
+		case e, ok = <-events:
+			switch e.EventType {
+			case EventPut:
+				err = Put(e.Key, e.Value)
+			case EventDelete:
+				err = Delete(e.Key)
+			default:
+				err = fmt.Errorf("unknown event type: %d", e.EventType)
+			}
+		}
+	}
+	if err != nil {
+		return fmt.Errorf("failed to read events from transaction log: %w", err)
+	}
+
+	logger.Run()
+	return err
+}
+
+///LOGGER/////
+
+var logger TransactionLogger
 
 // //////STORE//////////
 var ErrorNoSuchKey = errors.New("no such key")
@@ -63,6 +193,7 @@ func Delete(key string) error {
 		return ErrorNoSuchKey
 	}
 	delete(store.m, key)
+	logger.WriteDelete(key)
 	return nil
 }
 
@@ -82,6 +213,8 @@ func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	logger.WritePut(key, string(value))
 
 	w.WriteHeader(http.StatusCreated)
 }
@@ -104,6 +237,10 @@ func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(value))
 }
 func main() {
+	errInit := inizializeTransactionLogger()
+	if errInit != nil {
+		log.Fatalf("Failed to initialize transaction logger: %v", errInit)
+	}
 	r := mux.NewRouter()
 
 	r.HandleFunc("/v1/key/{key}", keyValuePutHandler).Methods("PUT")
